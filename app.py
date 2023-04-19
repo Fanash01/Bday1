2f72ef0356152e3d48c103dbed78b18328673023
Fanash01 <113841196+Fanash01@users.noreply.github.com>
2023-04-19 15:40:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
-import random
 import streamlit as st
+import random
 from PIL import Image
 
 def guess_number():
-    st.title("Guess the Number")
-    number = random.randint(1, 10)
-    guess = st.number_input("Guess a number between 1 and 10:", min_value=1, max_value=10)
-    if guess is not None:
-        if guess == number:
-            st.success("🎉🎂🎈 Happy Birthday! 🎈🎂🎉 May all your dreams come true and may this year bring you lots of joy, happiness and success. Enjoy your special day! 🎁🥳")
+    st.write("# Guess the number and get a birthday surprise!")
+    st.write("Choose a number between 1 and 10:")
+    number = st.number_input("", min_value=1, max_value=10, step=1, key="number_input")
+    if st.button("Guess"):
+        if random.randint(1, 3) == number:
+            st.write("## 🎉 Congratulations! You guessed the correct number. 🎉")
             balloons_img = Image.open("balloons.jpg")
-            st.image(balloons_img, caption="Balloons!", use_column_width=True)
-            st.write("Check out my personalized website at http://www.mywebsite.com")
-            st.audio("music.mp3", format="audio/mp3")
+            st.image(balloons_img, use_column_width=True)
         else:
-            st.warning("Wrong! Guess again.")
-
-guess_number()
+            st.write("Sorry, that's not the correct number. Try again!")
+            
+if __name__ == "__main__":
+    guess_number()
